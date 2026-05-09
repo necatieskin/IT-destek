30781dc9ab7dd216a926c1d2cbe92bcd67c6ca43
necatieskin <eskinnecati867@gmail.com>
2026-05-09 17:40:07 +0300

    spring boot projesi başlangıç dosyaları eklendi.

---
+++ src/main/java/com/itdestek/itdestek/ItDestekApplication.java
@@ -0,0 +1,13 @@
+package com.itdestek.itdestek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItDestekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItDestekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itdestek/itdestek/ItDestekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itdestek.itdestek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItDestekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
